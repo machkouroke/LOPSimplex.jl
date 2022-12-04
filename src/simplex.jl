b57ecfd03b76ae5382d7b50ba88cb483417de939
machkouroke <machkour20.mo1@gmail.com>
2022-12-04 21:32:45 +0100

    `build` Debug mode 7

--- src/simplex.jl
+++ src/simplex.jl
@@ -161,7 +161,7 @@ function simplex_py(A, b, c,
     inequality,
     type
     )
-    inequality = convert(Vector, inequality)
+    inequality = convert(Vector{String}, inequality)
     @show inequality
     # return simplex_case(A, b, c; inequality=inequality, type=type)
 end
